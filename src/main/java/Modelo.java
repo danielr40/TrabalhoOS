48d6b0415c38fbf69dd1ee8c67bc447b4199ba32
Felipe Bottrel <felbottrel@gmail.com>
2016-03-20 14:49:37 -0300

    remoção teste

--- src/main/java/Modelo.java
+++ src/main/java/Modelo.java
@@ -33,7 +33,8 @@ public class Modelo{
 	}
 
 	/**
-	 * Retorna os valores que multiplicam as variaveis na
+	 * Retorna os valores que multiplicam as variaveis na função objetivo.
+	 *
 	 * @return array com os valores da equação, na ordem das variaveis
 	 */
 	public double[] getZ(){
@@ -41,17 +42,36 @@ public class Modelo{
 	}
 
 	/**
-	 * Define os valores que multiplicam as variaveis
+	 * Define os valores que multiplicam as variaveis na função objetivo.
+	 *
 	 * @param z array com os valores da equação, na ordem das variaveis
 	 */
 	public void setZ(double[] z){
 		this.z = z;
 	}
 
+	/**
+	 * Retorna os valores das equações das restrições.
+	 * <p>
+	 * É uma matriz N x M, aonde N é o numero de restrições e M o número
+	 * de variaveis + 2 (O resultado da inequação). Ou seja, uma equação
+	 * <tt>2x + 3y >= 16</tt> se transofrma em <tt>[2, 3, 16]</tt>.
+	 *
+	 * @return matriz das restrições
+	 */
 	public double[][] getRestricoes(){
 		return restricoes;
 	}
 
+	/**
+	 * Define os valores das equações das restrições.
+	 * <p>
+	 * É uma matriz N x M, aonde N é o numero de restrições e M o número
+	 * de variaveis + 1 (O resultado da inequação). Ou seja, uma equação
+	 * <tt>2x + 3y >= 16</tt> se transofrma em <tt>[2, 3, 16]</tt>.
+	 *
+	 * @param restricoes
+	 */
 	public void setRestricoes(double[][] restricoes){
 		this.restricoes = restricoes;
 	}
@@ -75,22 +95,26 @@ public class Modelo{
 	}
 
 	/**
-	 * Prepara o modelo para ser colocado na matriz do
-	 * metodo Simplex.
+	 * Prepara o modelo para ser colocado na matriz do metodo Simplex.
 	 * <p>
 	 * Faz as seguintes operações:
-	 * <ul>
-	 *     <li>Multiplica <tt>minmax</tt> pelos valores de <tt>z</tt>.</li>
-	 *     <li></li>
-	 * </ul>
+	 * <ol>
+	 *   <li>Multiplica <tt>minmax</tt> pelos valores de <tt>z</tt>.</li>
+	 *   <li>Multiplica o valor da inaquação de uma restrição pelo resto da restrição.</li>
+	 *   <li>Coloca os valores na ordem que serão postos na matriz do Simplex.</li>
+	 * </ol>
 	 *
 	 * @return Modelo preparado
 	 */
-	public static Modelo prepareModelo(){
+	public static Modelo prepararModelo(){
 		int minmax = 0, numVar = 0, numRest = 0;
 		int[] ineqRest = new int[numRest];
 		double[] z = new double[numVar];
 		double[][] restricoes = new double[numRest][numVar+1];
+
+		/*
+		 * TODO: Receber modelo da interfaçe, parsear os valores, montar objeto modelo.
+		 */
 
 		return new Modelo(minmax, z, restricoes, numVar);
 	}
