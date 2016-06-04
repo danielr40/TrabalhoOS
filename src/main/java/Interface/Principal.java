cd35f4e6b6bb51aa83d25480f1ba0c68981883de
Hexmalun <mateus-parreiras@hotmail.com>
2016-06-04 15:14:09 -0300

    Interface

--- src/main/java/Interface/Principal.java
+++ src/main/java/Interface/Principal.java
@@ -394,12 +394,19 @@ public class Principal extends javax.swing.JFrame {
         
         for(int i=0;i<model.getRowCount();i++) {
             valor = Double.parseDouble(""+model.getValueAt(i,model.getColumnCount()-1));
-            operador = Double.parseDouble(""+model.getValueAt(i,model.getColumnCount()-2));
+            if((""+model.getValueAt(i,model.getColumnCount()-2)).equals("<")||
+                    (""+model.getValueAt(i,model.getColumnCount()-2)).equals("<=")){
+                operador = -1;
+            }else if((""+model.getValueAt(i,model.getColumnCount()-2)).equals(">")||
+                    (""+model.getValueAt(i,model.getColumnCount()-2)).equals(">=")){
+                operador = 1;
+            }else{
+                operador = 0;
+            }
             
             restricoes[i][0] = (operador>0) ? valor : valor*(-1);
             
-            for(int j=0;j<model.getColumnCount()-2;j++)
-            {
+            for(int j=0;j<model.getColumnCount()-2;j++){                
                 var = Double.parseDouble(""+model.getValueAt(i,j));
                 restricoes[i][j+1] = (operador>0) ? var : var*(-1); 
             }
